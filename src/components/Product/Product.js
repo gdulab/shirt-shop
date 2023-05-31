deacde6eb91399f0c9a312a39fefa629f377cdd5
gdulab <gdula.bartlomiej@gmail.com>
2023-05-31 13:01:41 +0200

    ADD useMemo function, FIX ProductForm css clean-up.

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,18 +1,18 @@
 import styles from './Product.module.scss';
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 import ProductImage from '../ProductImage/ProductImage';
 import ProductForm from '../ProductForm/ProductForm';
 import PropTypes from 'prop-types';
 
-
-
-const getPrice = (basePrice, additionalPrice) => {
-    return Math.floor(basePrice + additionalPrice);
-}
-
 const Product = props => {
     const [currentSize, setCurrentSize] = useState(props.sizes[0]);
     const [currentColor, setCurrentColor] = useState(props.colors[0]);
+
+    const getPrice = (basePrice, additionalPrice) => {
+        return Math.floor(basePrice + additionalPrice);
+    };
+
+    const price = useMemo(() => getPrice(props.basePrice, currentSize.additionalPrice), [props.basePrice, currentSize.additionalPrice]);
 
     return (
         <article className={styles.product}>
@@ -20,7 +20,7 @@ const Product = props => {
             <div>
                 <header>
                     <h2 className={styles.name}>{props.title}</h2>
-                    <span className={styles.price}>Price: ${getPrice(props.basePrice, currentSize.additionalPrice)}</span>
+                    <span className={styles.price}>Price: ${price}</span>
                 </header>
                 <ProductForm
                     title={props.title}
